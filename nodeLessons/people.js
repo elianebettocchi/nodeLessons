a299c8523fd7a686f9b38ad51db60c10db9401b6
Eliane Bettocchi <elianebettocchi@gmail.com>
2023-04-25 18:37:55 -0400

    Delete nodeLessons directory

--- nodeLessons/people.js
+++
@@ -1,4 +0,0 @@
-const people = ['eliane', 'alice', 'carlos'];
-const ages = [54, 17, 54]
-
-module.exports = {people, ages}
--- nodeLessons/test.js
+++
@@ -1,8 +0,0 @@
-//const name = "Alice";
-//console.log(name);
-
-const greet = (name)=>{
-    console.log(`hello ${name}`)
-}
-greet('eliane');
-greet('alice');
