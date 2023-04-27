7d66ca9d1e6716f107256dcf271cc0791a288fba
Eliane Bettocchi <elianebettocchi@gmail.com>
2023-04-26 20:28:53 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+app.set('view engine', 'ejs');
 app.listen(3000);
+app.use(morgan('dev'));
+app.use(express.static('public'))
+const blogs = [
+   {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+   {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+   {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
 app.get('/', (req,res)=>{
-    res.sendFile("./views/index.html",
-    {root: __dirname})
+    res.render('index', {title: 'Home', blogs});
 })
 app.get('/about', (req,res)=>{
-        res.sendFile("./views/about.html", {root: __dirname})
+        res.render('about', {title: "About"});
 })
-app.get('/about-us', (req,res)=>{
-    res.redirect("/about")
+app.get('/create', (req,res)=>{
+    res.render('create', {title: 'Create a new blog'});
 })
 app.use((req, res)=>{
-res.status(404).sendFile('./views/404.html', {root: __dirname})
+res.status(404).render('404', {title: '404'});
 })
+//app.use((req,res, next)=>{
+    //console.log('new request made:');
+    //console.log('host', req.hostname);
+    //console.log('path', req.path);
+    //console.log('method: ',req.method);
+    //next();
+//})
+//app.use((req,res, next)=>{
+    //console.log('new request made:');
+    //console.log('in the next middleware');
+//})
